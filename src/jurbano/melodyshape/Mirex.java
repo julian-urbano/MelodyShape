82de74b8b0cf07496add1cf84fcabecb32122b19
julian-urbano <urbano.julian@gmail.com>
2015-03-09 03:49:00 +0100

    Files updated since 2013 maintain the original 2013 year in the copyright, besides the new one.

--- src/jurbano/melodyshape/Mirex.java
+++ src/jurbano/melodyshape/Mirex.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2013  Julián Urbano <urbano.julian@gmail.com>
+// Copyright (C) 2014, 2015  Julián Urbano <urbano.julian@gmail.com>
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
